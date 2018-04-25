5bf7eae5071bac10a8a651c72fba1419492feb21
lincoln <henry.l.ayers@gmail.com>
2018-04-25 19:26:44 -0400

    added the parsing for the json string, add the textview references and then set the values to the textviews from the json parsing

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,24 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    // textviews for the sandwich data
+    private TextView tv_origin, tv_akaList,tv_description,tv_ingredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,12 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+      //  add references to the textviews for the sandwich data
+        tv_origin = findViewById(R.id.origin_tv);
+        tv_akaList = findViewById(R.id.also_known_tv);
+        tv_description = findViewById(R.id.description_tv);
+        tv_ingredients = findViewById(R.id.ingredients_tv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +56,13 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
+
                 .load(sandwich.getImage())
+                .error(R.drawable.image_not_found)
                 .into(ingredientsIv);
+
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +72,36 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        // sets the origin textview to the sandwich place of origin
+         tv_origin.setText(sandwich.getPlaceOfOrigin());
+        // set the description textview to the sandwich description
+         tv_description.setText(sandwich.getDescription());
+         // using string builder to get rid of the brackets and commas in the textview
+         StringBuilder mIngredientBuilder = new StringBuilder();
+         // getting list of ingredients from the sandwich to parse
+         List<String> mIngredients = sandwich.getIngredients();
+         //iterates thru each item and adds it the string with a carriage return at the end
+         for(String ingredient:mIngredients )
+         {
+             mIngredientBuilder.append(ingredient+"\n");
+         }
+         //get rid of last return- first get length
+        int length= mIngredientBuilder.length();
+        // delete the last character to get rid of white line below ingredients
+         mIngredientBuilder.deleteCharAt(length-1);
+         // sets the ingredients textview to the ingredients string builder
+        tv_ingredients.setText(mIngredientBuilder.toString());
+        // using string builder to get rid of the brackets and commas in the textview
+        StringBuilder mAkaListBuilder = new StringBuilder();
+        // getting list of aka names from the sandwich to parse
+         List<String > mAkaList = sandwich.getAlsoKnownAs();
+        //iterates thru each item and adds it the string with a carriage return at the end
+        for(String aka :mAkaList )
+        {
+            mAkaListBuilder.append(aka+"\n");
+        }
+        // sets the aka textview to the aka string builder
+        tv_akaList.setText(mAkaListBuilder.toString());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,78 @@
 package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.util.ArrayList;
 
-public class JsonUtils {
+public class JsonUtils
+{
+    public static Sandwich parseSandwichJson(String json)
+    {
+        Sandwich sandwich = new Sandwich();
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try
+        {
+            // get initial json object
+            org.json.JSONObject jObjectInitial = new org.json.JSONObject(json);
+           // get the name object from the initial object
+            org.json.JSONObject jObjectSandwichName = jObjectInitial.getJSONObject("name");
+            // get the mainName string from the sandwich object
+            String mSandwichName = jObjectSandwichName.getString("mainName");
+            // adds the main name to the sandwich object
+            sandwich.setMainName(mSandwichName);
+            // gets the sandwich alsoKnownAs name array
+            org.json.JSONArray mSandwichAkaArray = jObjectSandwichName.getJSONArray("alsoKnownAs");
+            // gets the sandwich ingredients list Array
+            org.json.JSONArray mIngredientsArray = jObjectInitial.getJSONArray("ingredients");
+            // converts the json aka Array to arrayList
+            ArrayList<String> akaList = convertJsonArray(mSandwichAkaArray);
+            // adds the aka list to the sandwich object
+            sandwich.setAlsoKnownAs(akaList);
+            // converts the Ingredients JsonArray to the arrayList
+            ArrayList<String> ingredientList = convertJsonArray(mIngredientsArray);
+            // adds the ingredient list to the sandwich object
+            sandwich.setIngredients(ingredientList);
+            // get place of Origin string from the jsonObject
+            String mOrigin = jObjectInitial.getString("placeOfOrigin");
+            // adds the placeOfOrigin to the sandwich object
+            sandwich.setPlaceOfOrigin(mOrigin);
+            // gets the description string from the jsonObject name
+             String mDescription = jObjectInitial.getString("description");
+            // adds the description to the sandwich object
+            sandwich.setDescription(mDescription);
+            //get image url from json object
+            String mImage = jObjectInitial.getString("image");
+            // adds image string to sandwich object
+            sandwich.setImage(mImage);
+        }
+           catch (org.json.JSONException error)
+        {
+                error.printStackTrace();
+        }
+
+        android.util.Log.v("JsonUtils",json);
+
+        return sandwich;
+    }
+
+    private static ArrayList<String> convertJsonArray(JSONArray jsonArray)
+    {
+        ArrayList<String> list = new ArrayList<>();
+            if (jsonArray != null)
+        {
+            for (int i=0;i<jsonArray.length();i++)
+            {
+                try
+                {
+                    list.add(jsonArray.getString(i));
+                }
+                    catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+          return list;
     }
 }
